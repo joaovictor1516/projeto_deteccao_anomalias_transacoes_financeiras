aa092ba5a9c6c574f6ea1f5c0ef7faf81f6f88dd
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-08-10 17:55:46 -0300

    remocao dos comentarios.

--- deteccao_anomalias_transacoes_financeiras.py
+++ deteccao_anomalias_transacoes_financeiras.py
@@ -7,13 +7,8 @@ scaler = StandardScaler()
 url = "https://storage.googleapis.com/download.tensorflow.org/data/creditcard.csv"
 df = pd.read_csv(url)
 
-#Class = 0 -> nao e fraudulenta
-#Class = 1 -> e fraudulenta
-
 proporcoes = df["Class"].value_counts(normalize=True)
 print(proporcoes)
-
-#feature engeneering e o processo de criar novas variaveis, ou normalizando variaveis ja exitentes, para ajudar no estudo da nossa ia
 
 df["Amount_log"] = np.log1p(df["Amount"]) #usamos o log para diminuir os valores da coluna
 
