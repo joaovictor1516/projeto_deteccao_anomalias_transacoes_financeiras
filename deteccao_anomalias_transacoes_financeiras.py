0c179fe4e972358cf5f3553bc218126d7a160762
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-08-11 11:31:43 -0300

    criacao do modelo de referencia para os demais modelos.

--- deteccao_anomalias_transacoes_financeiras.py
+++ deteccao_anomalias_transacoes_financeiras.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 scaler = StandardScaler()
@@ -21,3 +22,9 @@ y = df["Class"]
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, stratify=y, test_size=0.3, random_state=42
 )
+
+model = LogisticRegression(max_iter=1000)
+
+model.fit(x_train, y_train)
+
+y_predict = model.predict(x_test)
