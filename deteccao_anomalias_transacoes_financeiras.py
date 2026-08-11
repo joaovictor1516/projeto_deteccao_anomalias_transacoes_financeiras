27d542915dcaf0929aaa359df2ca087b614f93b1
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-08-11 11:11:50 -0300

    criacao do grupo de teste e do grupo de treino do modelo.

--- deteccao_anomalias_transacoes_financeiras.py
+++ deteccao_anomalias_transacoes_financeiras.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 scaler = StandardScaler()
 
@@ -13,3 +14,10 @@ print(proporcoes)
 df["Amount_log"] = np.log1p(df["Amount"])
 
 df["Amount_scaled"] = scaler.fit_transform(df[["Amount"]])
+
+x = df.drop("Class", axis=1)
+y = df["Class"]
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, stratify=y, test_size=0.3, random_state=42
+)
