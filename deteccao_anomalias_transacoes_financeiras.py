d782b98217baefd8ffe6e3b222214108f558d4ad
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-08-12 14:44:17 -0300

    adicao do modelo random forest.

--- deteccao_anomalias_transacoes_financeiras.py
+++ deteccao_anomalias_transacoes_financeiras.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from imblearn.over_sampling import SMOTE
 from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_recall_curve, classification_report, roc_curve, roc_auc_score
@@ -29,6 +30,7 @@ x_train_over, y_train_over = smote.fit_resample(
     y_train
 )
 
+# Logistic Regression:
 model = LogisticRegression(max_iter=10000)
 
 model.fit(x_train_over, y_train_over)
@@ -56,3 +58,21 @@ plt.title("Precision X Recall Curve")
 plt.xlabel("Recall")
 plt.ylabel("Precision")
 plt.show()
+
+# Random Forest:
+rf = RandomForestClassifier(
+    n_estimators=50,
+    max_depth=10,
+    n_jobs=1,
+    random_state=42
+)
+
+rf.fit(x_train_over, y_train_over)
+
+y_predict_rf = rf.predict(x_test)
+
+print(classification_report(y_test, y_predict_rf))
+
+y_probs_rf = rf.predict_proba(x_test)[:,1]
+
+print("AUC:",roc_auc_score(y_test, y_probs_rf))
